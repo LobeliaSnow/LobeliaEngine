3cb3b0c31638cdb449dfd10b60e3e03b7c26039b
雪瑠璃 <lobelia.snow@gmail.com>
2018-02-13 00:52:22 +0900

    バグ修正

--- Source/Scene/SceneMain.cpp
+++ Source/Scene/SceneMain.cpp
@@ -1,8 +1,42 @@
 #include "Lobelia.hpp"
 #include "SceneMain.hpp"
 
+//TODO : 描画ステートのパイプラインクラスを調整
+//TODO : コントローラー入力
+//TODO : テクスチャブレンドや雨等
+//TODO : トーン入出力処理(DXGI)を実装？
+//TODO : deltabaseでの飛び問題解決
+//Raypickする際に-1されていることを忘れてはならない。
+//TODO : Sound 矩形波を流し込めるようにしたりするのを簡単にする
+//TODO : DirectInput + XInput両刀で実装
+//TODO : キー入力の際、何かキーを押したかとる関数がほしい
+
+
 namespace Lobelia::Game {
 	SceneMain::SceneMain() :view(std::make_unique<Graphics::View>(Math::Vector2(), Application::GetInstance()->GetWindow()->GetSize())) {
+		Audio::Buffer buffer;
+		buffer.format.nChannels = 1;
+		buffer.format.nSamplesPerSec = 44100;
+		buffer.format.wBitsPerSample = 16;
+		buffer.format.nBlockAlign = s_cast<short>(buffer.format.wBitsPerSample / 8 * buffer.format.nChannels);
+		buffer.format.nAvgBytesPerSec = buffer.format.nSamplesPerSec*buffer.format.nBlockAlign;
+		buffer.format.wFormatTag = WAVE_FORMAT_PCM;
+		buffer.source = new BYTE[sizeof(short)*buffer.format.nAvgBytesPerSec];
+		buffer.size = sizeof(short)*buffer.format.nAvgBytesPerSec;
+		short* temp = r_cast<short*>(buffer.source);
+		//ラ
+		float hz = Audio::Device::CalcMIDIHz(69);
+		for (int i = 0; i < buffer.size / sizeof(short); i++) {
+			//音の波形のサイズは-32767.0~32767.0までなので、それを超えると波形がラリる可能性がある
+			//https://drumimicopy.com/audio-frequency/
+			temp[i] = s_cast<short>(32767.0f*sinf(2.0f*PI* hz *s_cast<float>(i) / s_cast<float>(buffer.format.nSamplesPerSec)));
+		}
+		Audio::EffectVoice::DisableEffect(0);
+		Audio::SourceVoice voice(buffer);
+		voice.Play(0);
+		while (voice.IsPlay());
+		voice.Stop();
+		delete[] buffer.source;
 	}
 	SceneMain::~SceneMain() {
 	}
